22a71eb943bf9297ba6b586a386f6d6c325658f0
Matthias <MatthiasSaihttam1@gmail.com>
2019-05-19 13:12:02 -0700

    Create Site Data JS File
    I need a list of posts as a JS file on Github so that I can load it on KA.
    This creates a Jekyll plugin that runs after the website is re-built, which takes data about the site and all the posts and exports them into a Javascript file.

---
+++ _plugins/siteDataJS.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+module Jekyll
+    Jekyll::Hooks.register :site, :post_write do |site|        
+        post_list = []
+        
+        site.posts.docs.map do |post|
+            post_hash = {
+                url: post.url,
+                date: post.data["date"],
+                slug: post.data["slug"],
+                title: post.data["title"],
+                excerpt: post.data["excerpt"]   
+            }
+            
+            post_list << post_hash
+        end
+        
+        site_data = {
+            url: site.config["url"] + site.baseurl,
+            title: site.config["title"],
+            description: site.config["description"],
+            github_url: site.config["github"]["repository_url"],
+            posts: post_list
+        }
+        
+        File.write('_site/loadSiteData.js', "handleSiteData(#{site_data.to_json})");
+    end
+end
